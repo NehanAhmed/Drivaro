030449a9f49ad02f20831c8751fb1012607d024b
Nehan Ahmed <nehanahmed2k23@gmail.com>
2026-01-19 10:59:07 +0500

    fixed the error

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/client";
 import { customSessionClient } from "better-auth/client/plugins";
 import type { auth } from "@/lib/auth";
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
   plugins: [customSessionClient<typeof auth>()],
 
 });
